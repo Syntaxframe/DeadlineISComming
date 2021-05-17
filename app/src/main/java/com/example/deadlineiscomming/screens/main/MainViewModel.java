83a030232bed2572258c4f717a3db01e703c0058
Syntaxframe <niiaz280228@gmail.com>
2021-05-17 23:00:13 +0400

    Initial commit

---
+++ app/src/main/java/com/example/deadlineiscomming/screens/main/MainViewModel.java
@@ -0,0 +1,17 @@
+package com.example.deadlineiscomming.screens.main;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.deadlineiscomming.App;
+import com.example.deadlineiscomming.model.Note;
+
+import java.util.List;
+
+public class MainViewModel extends ViewModel {
+    private LiveData<List<Note>> noteLiveData = App.getInstance().getNoteDao().getAllLiveData();
+
+    public LiveData<List<Note>> getNoteLiveData() {
+        return noteLiveData;
+    }
+}
